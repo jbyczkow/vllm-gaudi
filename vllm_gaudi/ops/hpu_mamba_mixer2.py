eb4d1de6679a286e11696c09349a7a51242bd97a
Jakub Byczkowski <jbyczkowski@habana.ai>
2026-01-23 17:10:11 +0200

    Remove mamba cache print

--- vllm_gaudi/ops/hpu_mamba_mixer2.py
+++ vllm_gaudi/ops/hpu_mamba_mixer2.py
@@ -468,7 +468,6 @@ class HPUMambaMixer2(MambaMixer2):
         if attn_metadata is not None:
             self_kv_cache = self.kv_cache[forward_context.virtual_engine]
             # conv_state = (..., dim, width-1) yet contiguous along 'dim'
-            print(f"{self_kv_cache[0]=} {self_kv_cache[1]=}")
             conv_state = self_kv_cache[0].transpose(-1, -2)
             ssm_state = self_kv_cache[1]
             state_indices_tensor = attn_metadata.state_indices_tensor
